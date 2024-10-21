b61135d7046876400bac6d1b6116899ba47a53bc
Scott Dixon <js.dixon@gmail.com>
2024-10-20 19:49:59 -0700

    Add vial support for hhkb_lite2

---
+++ keyboards/hhkb_lite_2/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/*SPDX - License - Identifier : GPL - 2.0 - or -later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID \
+    { 0xC2, 0x14, 0x4F, 0x20, 0x0A, 0x9A, 0xEA, 0xFA }
+
+#define VIAL_UNLOCK_COMBO_ROWS  { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS  { 0, 1 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
